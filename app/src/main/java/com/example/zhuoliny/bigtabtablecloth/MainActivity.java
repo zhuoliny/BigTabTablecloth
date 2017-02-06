7908d7b0d42089d4f830feadfa4f597685e21d4b
zhuoliny <zhuoliny@buffalo.edu>
2017-02-05 20:59:46 -0500

    Update001 Just Buttons and Stuff

--- app/src/main/java/com/example/zhuoliny/bigtabtablecloth/MainActivity.java
+++ app/src/main/java/com/example/zhuoliny/bigtabtablecloth/MainActivity.java
@@ -1,13 +1,35 @@
 package com.example.zhuoliny.bigtabtablecloth;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button square;
+    Button circle;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        square = (Button) findViewById(R.id.SquareClick);
+        square.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intentS = new Intent(getApplicationContext(), SqureClick.class);
+                startActivity(intentS);
+            }
+        });
+
+        circle = (Button) findViewById(R.id.CircleClick);
+        circle.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intentC = new Intent(getApplicationContext(), CircleClick.class);
+                startActivity(intentC);
+            }
+        });
     }
 }
